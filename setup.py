d8e2009c4a5aefe81c350c72f5eb1630082389a7
Davide Moro <davide.moro@redomino.com>
2013-06-05 14:23:06 +0200

    added test requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from distutils.core import setup
 
 VERSION = '2.2.6'
 
+tests_require = ['nose']
+
 setup(
     name='webexpy',
     version=VERSION,
@@ -13,4 +15,8 @@ setup(
     download_url='https://github.com/prior/webexpy/tarball/v%s'%VERSION,
     packages=['webex','webex.test'],
     install_requires=['lxml', 'requests<=0.12.1', 'sanetime', 'utilspy', 'gevent'],
+    tests_require=tests_require,
+    extras_require = {
+                      'test': tests_require,
+                     },
 )
